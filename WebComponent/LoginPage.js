aa2570d2a30b2f695836979893002a6b2425d313
aridwan1037 <aridwan1037@gmail.com>
2024-12-28 22:28:32 +0700

    practice negative test

--- WebComponent/LoginPage.js
+++ WebComponent/LoginPage.js
@@ -7,7 +7,7 @@ class LoginPage {
     this.usernameInput = By.id('user-name')
     this.passwordInput = By.xpath("//input[@id='password']")
     this.loginButton = By.xpath("//input[@id='login-button']")
-    // this.errorMessage = By.css('error-message-container')
+    this.errorMessage = By.css('.error-message-container')
   }
 
   async navigate() {
@@ -18,6 +18,15 @@ class LoginPage {
     await this.driver.findElement(this.usernameInput).sendKeys(username)
     await this.driver.findElement(this.passwordInput).sendKeys(password)
     await this.driver.findElement(this.loginButton).click()
+  }
+  
+  async getErrorMessage(){
+    try {
+        const errorElement = await this.driver.findElement(this.errorMessage)
+        return await errorElement.getText()
+    } catch (err) {
+        return null
+    }
   }
 
 }
